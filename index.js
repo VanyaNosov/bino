442929f15f0574e460a5eff16ca1de38d8522166
vanyanosov <nosov582125@gmail.com>
2019-03-03 12:59:25 +0200

    header_top

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-   
+var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+const header_top = document.getElementById('header_top')
+
+function vanya () {
+    if (scrollTop > 820) {
+        header_top.style.top = "0%"; 
+        header_top.style.opacity = "1";
+        console.log(header_top);
+        console.log(scrollTop);
+    }else {
+        header_top.style.top = "-92%"; 
+        header_top.style.opacity = "0";
+    }
+};
+vanya();
+
