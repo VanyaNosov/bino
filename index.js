94bcba3505e49011e98ee82d4f361daf37330dc2
vanyanosov <nosov582125@gmail.com>
2019-03-04 19:46:01 +0200

    bino_vanyassss

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-const header_top = document.getElementById('header_top').addEventListener("")
+const header_top = document.getElementById('header_top');
+window.addEventListener("scroll",() => vanya());
 
 function vanya () {
-    if (scrollTop > 820) {
+    if (window.pageYOffset > 820) {
         header_top.style.top = "0%"; 
         header_top.style.opacity = "1";
-        console.log(header_top);
-        console.log(scrollTop);
     }else {
         header_top.style.top = "-92%"; 
         header_top.style.opacity = "0";
     }
 };
 
-vanya();
+
+
+
+
+
+
 
