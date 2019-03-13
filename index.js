134379db22123a5ea3efb61d933e7ac23016a9e9
vanyanosov <nosov582125@gmail.com>
2019-03-13 20:28:26 +0200

    slider(no no no no)

--- index.js
+++ index.js
@@ -11,6 +11,28 @@ function vanya () {
     }
 };
 
+let vanyac = document.getElementById('buttom_slider1').onclick = scrollBottom;
+let vanyac2 = document.getElementById('buttom_slider2').onclick = scrollBottom_bottom;
+
+let bop = 0;
+
+function scrollBottom () {
+    const vanyacha = document.getElementById('content');
+    bop = bop  - 250;
+    vanyac = vanyacha.style.top = bop + 'px';
+}
+
+let bottom = 0;
+
+function scrollBottom_bottom () {
+    const vanyacha = document.getElementById('content');
+    bottom = bottom - 250;
+    vanyac2 = vanyacha.style.bottom = bottom + 'px';
+}
+
+
+
+
 
 
 
