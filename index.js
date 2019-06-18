d24f011eac8f720930b21d121b0ff8279a907633
vanyanosov <nosov582125@gmail.com>
2019-06-18 23:37:09 +0300

    Issues N1, N2

--- index.js
+++ index.js
@@ -19,79 +19,100 @@ let Input = document.getElementsByClassName('info_check_in');
 let inputNam = document.getElementById("Name");
 let inputEm = document.getElementById("Email");
 
-buttom_top1.addEventListener('click', () => {
-  setStyle('buttom_top1', '0.8s', '0px');
-});
-
-buttom_top2.addEventListener('click', () => {  
-  setStyle('buttom_top2', '0.4s', '-500px');
-});
-
-buttom_top3.addEventListener('click', () => {
-  setStyle('buttom_top3', '0.8s', '-1000px');
-});
-
+const style = {
+    colors: {
+      dark_grey: '#3e3e3e',
+      bright_red: '#e74c3c',
+      light_grayish_blue: '#f4f5f9',
+    },
+};
 const setStyle = (eventOnButton, transition, top) => {
-  switch(eventOnButton) {
+    const {colors} = style;
+    const {dark_grey, bright_red, light_grayish_blue } = colors;
+    switch(eventOnButton) {
       case 'buttom_top3':
-          buttom_slider_c3.style.backgroundColor = 'e74c3c';
-          buttom_slider_c2.style.backgroundColor = '#3e3e3e';
-          buttom_slider_c1.style.backgroundColor = '#3e3e3e'; 
-          vanyacha.style.transition = transition;
-          vanyacha.style.top = top;
-          break;
+        buttom_slider_c3.style.backgroundColor = bright_red;
+        buttom_slider_c2.style.backgroundColor = dark_grey;
+        buttom_slider_c1.style.backgroundColor = dark_grey; 
+        vanyacha.style.transition = transition;
+        vanyacha.style.top = top;
+        break;
       case 'buttom_top2':
-          buttom_slider_c2.style.backgroundColor = 'e74c3c';
-          buttom_slider_c1.style.backgroundColor = '#3e3e3e';
-          buttom_slider_c3.style.backgroundColor = '#3e3e3e';
-          vanyacha.style.transition = transition;
-          vanyacha.style.top = top;
-          break;
+        buttom_slider_c2.style.backgroundColor = bright_red;
+        buttom_slider_c1.style.backgroundColor = dark_grey;
+        buttom_slider_c3.style.backgroundColor = dark_grey;
+        vanyacha.style.transition = transition;
+        vanyacha.style.top = top;
+        break;
       case 'buttom_top1':
-          buttom_slider_c1.style.backgroundColor = 'e74c3c';
-          buttom_slider_c2.style.backgroundColor = '#3e3e3e';
-          buttom_slider_c3.style.backgroundColor = '#3e3e3e';
-          vanyacha.style.transition = transition;
-          vanyacha.style.top = top;
-          break;
-  }
+        buttom_slider_c1.style.backgroundColor = bright_red;
+        buttom_slider_c2.style.backgroundColor = dark_grey;
+        buttom_slider_c3.style.backgroundColor = dark_grey;
+        vanyacha.style.transition = transition;
+        vanyacha.style.top = top;
+        break;
+      case 'slid_content_one':
+        OneSlid.style.backgroundColor = bright_red;
+        TwoSlid.style.backgroundColor = light_grayish_blue;
+        ThreeSlid.style.backgroundColor = light_grayish_blue;
+        slidContentOne2.style.opacity = '1';
+        slidContentTwo.style.opacity = '0';
+        slidContentThree.style.opacity = '0';    
+    }
+};
+  
+buttom_top1.addEventListener('click', () => {
+    setStyle('buttom_top1', 0.8, '0px');
+});
+  
+buttom_top2.addEventListener('click', () => {  
+    setStyle('buttom_top2', 0.4, '-500px');
+});
+  
+buttom_top3.addEventListener('click', () => {
+    setStyle('buttom_top3', 0.8, '-1000px');
+});
+
+function sliderStyles () {
+    const {colors} = style;
+    const {bright_red, light_grayish_blue } = colors;
+    slid_content_one.addEventListener('click', function () {
+        OneSlid.style.backgroundColor = bright_red;
+        TwoSlid.style.backgroundColor = light_grayish_blue;
+        ThreeSlid.style.backgroundColor = '#f4f5f9';
+        slidContentOne2.style.opacity = "1";
+        slidContentTwo.style.opacity = "0";
+        slidContentThree.style.opacity = "0";
+        slidContentOne2.style.transition = 1 + 's';
+        slidContentTwo.style.transition = 0 + 's';
+        slidContentThree.style.transition = 0 + 's';
+    }); 
+             
+    slid_content_two.addEventListener('click', function () {
+        TwoSlid.style.backgroundColor = bright_red;
+        OneSlid.style.backgroundColor = light_grayish_blue;
+        ThreeSlid.style.backgroundColor = light_grayish_blue;
+        slidContentOne2.style.opacity = '0';
+        slidContentThree.style.opacity = "0";
+        slidContentTwo.style.opacity = "1";
+        slidContentOne2.style.transition = 0 + 's';
+        slidContentTwo.style.transition = 1 + 's';
+        slidContentThree.style.transition = 0 + 's';
+    });
+    
+    slid_content_three.addEventListener('click', function () { 
+        ThreeSlid.style.backgroundColor = bright_red;
+        TwoSlid.style.backgroundColor = light_grayish_blue;
+        OneSlid.style.backgroundColor = light_grayish_blue;
+        slidContentOne2.style.opacity = '0';
+        slidContentThree.style.opacity = "1";
+        slidContentTwo.style.opacity = "0";
+        slidContentOne2.style.transition = 0 + 's';
+        slidContentTwo.style.transition = 0 + 's';
+        slidContentThree.style.transition = 1 + 's';
+    });
 }
-
-slid_content_one.addEventListener('click', function () {
-    OneSlid.style.backgroundColor = 'e74c3c';
-    TwoSlid.style.backgroundColor = '#f4f5f9';
-    ThreeSlid.style.backgroundColor = '#f4f5f9';
-    slidContentOne2.style.opacity = "1";
-    slidContentTwo.style.opacity = "0";
-    slidContentThree.style.opacity = "0";
-    slidContentOne2.style.transition = 1 + 's';
-    slidContentTwo.style.transition = 0 + 's';
-    slidContentThree.style.transition = 0 + 's';
-}); 
-         
-slid_content_two.addEventListener('click', function () {
-    TwoSlid.style.backgroundColor = 'e74c3c';
-    OneSlid.style.backgroundColor = '#f4f5f9';
-    ThreeSlid.style.backgroundColor = '#f4f5f9';
-    slidContentOne2.style.opacity = '0';
-    slidContentThree.style.opacity = "0";
-    slidContentTwo.style.opacity = "1";
-    slidContentOne2.style.transition = 0 + 's';
-    slidContentTwo.style.transition = 1 + 's';
-    slidContentThree.style.transition = 0 + 's';
-});
-
-slid_content_three.addEventListener('click', function () { 
-    ThreeSlid.style.backgroundColor = 'e74c3c';
-    TwoSlid.style.backgroundColor = '#f4f5f9';
-    OneSlid.style.backgroundColor = '#f4f5f9';
-    slidContentOne2.style.opacity = '0';
-    slidContentThree.style.opacity = "1";
-    slidContentTwo.style.opacity = "0";
-    slidContentOne2.style.transition = 0 + 's';
-    slidContentTwo.style.transition = 0 + 's';
-    slidContentThree.style.transition = 1 + 's';
-});
+sliderStyles();
 
 const header_top = document.getElementById('header_top');
 
@@ -112,16 +133,12 @@ function ValidatorEmail (email) {
 }
 
 function InputValidator (e) {
-    if(e.Name.selectionEnd > 5 && ValidatorEmail(e.Email.value)) {
+    if (ValidatorEmail(e.Email.value) && e.Name.selectionEnd > 5) {
         DisabledButton.style.backgroundColor = 'red';
-    }else {
-        DisabledButton.style.backgroundColor = '#C0C0C0';
-    }
-  
-    if(ValidatorEmail(e.Email.value) && e.Name.selectionEnd > 5) {
         inputEm.style.borderBottom = '2px solid #32CD32';
         inputNam.style.borderBottom = '2px solid #32CD32';
-    }else {
+    } else {
+        DisabledButton.style.backgroundColor = '#C0C0C0';
         inputNam.style.borderBottom = '2px solid #C0C0C0';
         inputEm.style.borderBottom = '2px solid #C0C0C0';
     }
