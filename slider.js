3ec96e497e58b13e1d1464cf1fb154be4cca83fe
vanyanosov <nosov582125@gmail.com>
2019-06-18 23:20:17 +0300

    slid-one

--- slider.js
+++ slider.js
@@ -2,7 +2,6 @@ const textSlidBlockOne = document.getElementById('slid_text_one');
 const textSlidBlockTwo = document.getElementById('slid_text_two');
 const slidBottomRight = document.getElementById('slid_button_block_right');
 slidBottomRight.addEventListener('click', () => {
-  textSlidBlockTwo.style.display = 'inline'
   textSlidBlockOne.style.left = '-500px';
   textSlidBlockTwo.style.left = '25%';
 })
