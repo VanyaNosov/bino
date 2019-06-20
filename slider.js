a50284df3d50e104a185022147f19340a7ca888e
vanyanosov <nosov582125@gmail.com>
2019-06-20 19:00:21 +0300

    slid-one

--- slider.js
+++ slider.js
@@ -1,7 +1,18 @@
 const textSlidBlockOne = document.getElementById('slid_text_one');
 const textSlidBlockTwo = document.getElementById('slid_text_two');
 const slidBottomRight = document.getElementById('slid_button_block_right');
+let dd = false;
 slidBottomRight.addEventListener('click', () => {
-  textSlidBlockOne.style.left = '-500px';
-  textSlidBlockTwo.style.left = '25%';
+  if (!dd) {
+    textSlidBlockOne.style.left = '-500px';
+    textSlidBlockTwo.style.left = '25%';
+    textSlidBlockOne.style.left = '100%';
+    dd = true;
+    return;
+  }else {
+    textSlidBlockTwo.style.left = '-500px';
+    textSlidBlockOne.style.left = '25%'; 
+    dd = false;
+    return;
+  }
 })
