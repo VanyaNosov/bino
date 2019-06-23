9fd5f498dc1aa5d70325b326cc3bdff9ce7764be
vanyanosov <nosov582125@gmail.com>
2019-06-23 14:03:16 +0300

    s

--- slider.js
+++ slider.js
@@ -1,18 +1,17 @@
 const textSlidBlockOne = document.getElementById('slid_text_one');
 const textSlidBlockTwo = document.getElementById('slid_text_two');
 const slidBottomRight = document.getElementById('slid_button_block_right');
-let dd = false;
+let slid = false;
 slidBottomRight.addEventListener('click', () => {
-  if (!dd) {
+  if (!slid) {
     textSlidBlockOne.style.left = '-500px';
     textSlidBlockTwo.style.left = '25%';
-    textSlidBlockOne.style.left = '100%';
-    dd = true;
+    slid = true;
     return;
   }else {
     textSlidBlockTwo.style.left = '-500px';
     textSlidBlockOne.style.left = '25%'; 
-    dd = false;
+    slid = false;
     return;
   }
 })
