b7806469bb535126175c2d8d0f0a1351aec3be2e
vanyanosov <nosov582125@gmail.com>
2019-08-14 18:52:38 +0300

    func remove,buttonLeft slid

--- slider.js
+++ slider.js
@@ -1,39 +1,86 @@
-const textSlidBlockOne = document.getElementById('slid_text_one');
-const textSlidBlockTwo = document.getElementById('slid_text_two');
-// const textSlidBlockTwo2 = document.getElementById('slid_text_two-2');
+let textSlidBlockOne = document.getElementById('slid_text_one');
+let textSlidBlockTwo = document.getElementById('slid_text_two');
 const slidContent = document.getElementById('slid_content');
-// const textSlidBlockOneClone = document.getElementById('slid_text_one_clone');
-// const textSlidBlockOneClone = textSlidBlockOne.cloneNode(textSlidBlockTwo);
-// const insertBefore = slidContent.insertBefore(textSlidBlockOne, textSlidBlockTwo);
-
+const slidBottomLeft = document.getElementById('slid_button_block_left');
 const slidBottomRight = document.getElementById('slid_button_block_right');
+const textSlidBlockOneClone = textSlidBlockOne.cloneNode(true);
+const textSlidBlockTwoClone = textSlidBlockTwo.cloneNode(true);
+const addItemOne = slidContent.appendChild(textSlidBlockOneClone)
+const addItemTwo = slidContent.appendChild(textSlidBlockTwoClone)
 let slid = false;
-
-function slidStarter(ONE) {
+addItemOne.id = 'slidOneClone';
+addItemTwo.id = 'slidTwoClone';
+addItemOne.style.left = '-75%';
+addItemOne.style.top = '50%'
+addItemTwo.style.left = '-75%';
+addItemTwo.style.top = '50%'
+function slidStarterRight(ONE) {
+  const slidArray = document.getElementsByClassName('text_slid_block');
   if(ONE === 1) {
-    slidContent.insertBefore(textSlidBlockTwo, textSlidBlockOne);
+    slidContent.insertBefore(textSlidBlockTwo, slidArray[0]);
   }
   if(ONE === 2) {  
-      slidContent.insertBefore(textSlidBlockOne, textSlidBlockTwo);
+    slidContent.insertBefore(textSlidBlockOne, slidArray[0]);
   }
 }
 
-slidBottomRight.addEventListener('click', () => {
-      if(!slid) {
-        slidStarter(1)
-        textSlidBlockOne.style.left = '-100%';
-        textSlidBlockTwo.style.left = '25%'; 
-        slid = true;
-      return;
-    }else {
-        slidStarter(2)
-        textSlidBlockTwo.style.left = '100%';
-        textSlidBlockOne.style.left = '25%'; 
-        slid = false;
-        return;
-    }
-  })
+function slidStarterLeft(ONE) {
+  const slidArray = document.getElementsByClassName('text_slid_block');
+  if(ONE === 3) {
+    slidContent.insertBefore(addItemTwo, slidArray[3]);
+  }
 
+  if(ONE === 2) {  
+    slidContent.insertBefore(addItemOne, slidArray[4]);
+  }
+}
+
+function deoOne() {
+  slidContent.removeChild(textSlidBlockOne)
+}
+
+slidBottomRight.addEventListener('click', () => {
+  if(!slid) {
+    slidStarterRight(1)
+    textSlidBlockOne.style.left = '-100%';
+    textSlidBlockTwo.style.left = '25%'; 
+    slid = true;
+    return;
+  }else {
+    slidStarterRight(2)
+    textSlidBlockTwo.style.left = '-100%';
+    textSlidBlockOne.style.left = '25%'; 
+    slid = false;
+    return;
+  }
+})
+
+  
+slidBottomLeft.addEventListener('click', () => {
+  if(!slid) {
+    slidStarterLeft(3)
+    deoOne()
+    addItemOne.style.left = '100%';
+    addItemTwo.style.left = '25%'; 
+    slid = true;
+    return;
+  }else {
+    slidStarterLeft(2)
+    deoOne()
+    addItemTwo.style.left = '100%';
+    addItemOne.style.left = '25%'; 
+    slid = false;
+    return;
+  }
+})
+
+
+
+
+// const textSlidBlockTwo2 = document.getElementById('slid_text_two-2');
+// const textSlidBlockOneClone = document.getElementById('slid_text_one_clone');
+
+// const insertBefore = slidContent.insertBefore(textSlidBlockOne, textSlidBlockTwo);
 // let yd;
 // let pd;
 // function dasd () {
