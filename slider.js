cc141c6b33e6e70b5b6178da10fa58c4b3c584d0
vanyanosov <nosov582125@gmail.com>
2019-08-22 20:56:44 +0300

    Issues 5-6

--- slider.js
+++ slider.js
@@ -27,27 +27,38 @@ function slidStarterRight(ONE) {
 function slidStarterLeft(ONE) {
   const slidArray = document.getElementsByClassName('text_slid_block');
   if(ONE === 3) {
-    slidContent.insertBefore(addItemTwo, slidArray[3]);
+    slidContent.insertBefore(addItemTwo, slidArray[0]);
   }
 
-  if(ONE === 2) {  
-    slidContent.insertBefore(addItemOne, slidArray[4]);
+  if(ONE === 4) {  
+    slidContent.insertBefore(addItemOne, slidArray[0]);
   }
 }
 
-function deoOne() {
+function delOneContainer () {
+  slidContent.removeChild(addItemOne)
+  slidContent.removeChild(addItemTwo)
+}
+
+function delTwoContainer () {
   slidContent.removeChild(textSlidBlockOne)
+  slidContent.removeChild(textSlidBlockTwo)
 }
+
 
 slidBottomRight.addEventListener('click', () => {
   if(!slid) {
     slidStarterRight(1)
+    // delTwoContainer ()
+    // deoTwo()
     textSlidBlockOne.style.left = '-100%';
     textSlidBlockTwo.style.left = '25%'; 
     slid = true;
     return;
   }else {
     slidStarterRight(2)
+    // delTwoContainer ()
+    // deoTwo()
     textSlidBlockTwo.style.left = '-100%';
     textSlidBlockOne.style.left = '25%'; 
     slid = false;
@@ -55,18 +66,17 @@ slidBottomRight.addEventListener('click', () => {
   }
 })
 
-  
 slidBottomLeft.addEventListener('click', () => {
   if(!slid) {
     slidStarterLeft(3)
-    deoOne()
+    // deoOne()
     addItemOne.style.left = '100%';
     addItemTwo.style.left = '25%'; 
     slid = true;
     return;
   }else {
-    slidStarterLeft(2)
-    deoOne()
+    slidStarterLeft(4)
+    // deoOne()
     addItemTwo.style.left = '100%';
     addItemOne.style.left = '25%'; 
     slid = false;
@@ -75,7 +85,50 @@ slidBottomLeft.addEventListener('click', () => {
 })
 
 
+// function delTwoContainer () {
+//   const slidArray = document.getElementsByClassName('text_slid_block');
+//   if(slidArray[0] === addItemOne) {
+//     textSlidBlockTwo.style.display = 'none';
+//     addItemTwo.style.display = 'inline';
+//     addItemTwo.style.left = '25%';
+//     slidContent.insertBefore(addItemTwo, slidArray[3]);
+//   }else {
+//     textSlidBlockOne.style.display = 'none';
+//     addItemOne.style.display = 'inline';
+//     addItemOne.style.left = '25%';
+//     slidContent.insertBefore(addItemOne, slidArray[4]);
+//   }
+// }
 
+// function deoOne() {
+//   const slidArray = document.getElementsByClassName('text_slid_block');
+//   if(slidArray[1] || slidArray[2] === textSlidBlockOne) {
+//     textSlidBlockOne.style.display = 'none';
+//     addItemOne.style.display = 'inline';
+//     addItemOne.style.left = '25%'; 
+//     slidContent.insertBefore(addItemOne, textSlidBlockOne);
+//   }else {
+//     textSlidBlockTwo.style.display = 'none';
+//     addItemTwo.style.display = 'inline';
+//     addItemTwo.style.left = '25%'; 
+//     slidContent.insertBefore(addItemTwo, textSlidBlockTwo);
+//   }
+// }
+
+// function deoTwo() {
+//   const slidArray = document.getElementsByClassName('text_slid_block');
+//   if(slidArray[3] || slidArray[4] === addItemOne) {
+//     addItemOne.style.display = 'none';
+//     textSlidBlockOne.style.display = 'inline';
+//     textSlidBlockOne.style.left = '25%';
+//     slidContent.insertBefore(textSlidBlockOne, addItemOne);
+//   }else {
+//     textSlidBlockTwo.style.display = 'inline';
+//     addItemTwo.style.display = 'none';
+//     addItemTwo.style.left = '25%'; 
+//     slidContent.insertBefore(textSlidBlockTwo, addItemTwo);
+//   }
+// }
 
 // const textSlidBlockTwo2 = document.getElementById('slid_text_two-2');
 // const textSlidBlockOneClone = document.getElementById('slid_text_one_clone');
