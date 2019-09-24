320e3ebbd80157897bbc45b0171d9effbacc863b
vanyanosov <nosov582125@gmail.com>
2019-09-24 18:02:03 +0300

    ne vem

--- slider.js
+++ slider.js
@@ -1,58 +1,36 @@
-let textSlidBlockOne = document.getElementById('slid_text_one');
-let textSlidBlockTwo = document.getElementById('slid_text_two');
-let textSlidBlockthree = document.getElementById('slid_text_three ');
-let textSlidBlockfoure = document.getElementById('slid_text_foure');
-let textSlidBlockfive = document.getElementById('slid_text_five ');
 const slidContent = document.getElementById('slid_content');
 const slidBottomLeft = document.getElementById('slid_button_block_left');
 const slidBottomRight = document.getElementById('slid_button_block_right');
 const index = 0;
 
-// function leftElem () {
-//   const cloneOne = textSlidBlockTwo.cloneNode(true);
-//   const slidArray = document.getElementsByClassName('text_slid_block');
-//   slidContent.insertBefore(cloneOne, slidArray[0]);
-// }
+function addingFuncOne () {
+  const slidArray = document.getElementsByClassName('text_slid_block');
+  let currentItem = slidArray[0];
+  slidContent.insertBefore(currentItem, slidArray[slidArray[0] + 1]);
+  currentItem = slidArray[0];
+  return;
+}
 
-// function leftMovement (direction, slide) {
-//   const slidArray = document.getElementsByClassName('text_slid_block');
-//   const cloneOne = slide.cloneNode(true);
-//   if(direction === 'back') {
-//     slidContent.removeChild(slidArray[0]);
-//     slidContent.appendChild(cloneOne);
-//   }else {
-//     slidContent.removeChild(slidArray[slidArray.length - 1])
-//     slidContent.insertBefore(cloneOne, slidArray[0]);
-//   }
-// }
-
-// function fdf (direction) {
-//   const slidArray = document.getElementsByClassName('text_slid_block');
-
-// }
-
-
-// slidBottomLeft.addEventListener('click', () => {
-//   console.log('fd')
-// })
-
-// leftElem ()
-
+function addingFuncTwo () {
+  const slidArray = document.getElementsByClassName('text_slid_block');
+  let currentItem = slidArray[0];
+  slidContent.insertBefore(currentItem, slidArray[slidArray[0] - 1]);
+  currentItem = slidArray[0];
+  return;
+}
 
 
 function funcDistributorOne (direction) {
   const slidArray = document.getElementsByClassName('text_slid_block');
   let currentItem = slidArray[0];
-    if (slidArray[0] === currentItem && direction === 'right') {
-      if (index === slidArray.length - 1) {
-        slidContent.insertBefore(currentItem, slidArray[0]);
-        currentItem = slidArray[0];
-        return;
-      }
-      slidContent.insertBefore(currentItem, slidArray[slidArray[0] + 1]);
+  if (slidArray[0] === currentItem && direction === 'right') {
+    if (index === slidArray.length - 1) {
+      slidContent.insertBefore(currentItem, slidArray[0]);
       currentItem = slidArray[0];
       return;
     }
+    addingFuncOne ()
+  }
 }
 
 function funcDistributorTwo (direction) {
@@ -64,19 +42,15 @@ function funcDistributorTwo (direction) {
       currentItem = slidArray[slidArray.length - 1];
       return;
     }
-    slidContent.insertBefore(currentItem, slidArray[slidArray[0] - 1]);
-    currentItem = slidArray[0];
-    return;
+    addingFuncTwo ()
   }
 }
 
 slidBottomRight.addEventListener('click', () => {
-  // leftMovement('back', textSlidBlockOne);
   funcDistributorOne('right')
 })
 
 slidBottomLeft.addEventListener('click', () => {
-  // leftMovement('back', textSlidBlockOne);
   funcDistributorTwo('left')
 });
 
